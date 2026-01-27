56315ce58331b4d33e71add8ddd39cc95f49e6bc
Joel Gantshua <joelgantshua@gmail.com>
2026-01-28 00:43:20 +0100

    Last

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -73,7 +73,7 @@ export default function Contact() {
 
   const sendWhatsApp = (e) => {
     e.preventDefault();
-    const phoneNumber = "+212669444701"; // Numéro sans espaces ni caractères spéciaux
+    const phoneNumber = "+33669005470"; // Numéro sans espaces ni caractères spéciaux
     const message = encodeURIComponent(
       `Bonjour, je m'appelle ${formData.name}.
 
@@ -81,7 +81,7 @@ export default function Contact() {
       `Je suis intéressé(e) par le service : ${formData.service}.
 
 ` +
-      `${formData.message ? `Mon message : ${formData.message}` : ''}
+      `${formData.message ? `: ${formData.message}` : ''}
 
 ` +
       `Email : ${formData.email}`
